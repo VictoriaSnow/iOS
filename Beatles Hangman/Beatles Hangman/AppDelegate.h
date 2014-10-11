640ddd2413a502d5f9058b0129b4acc256bc3620
Victoria <victoriasnow1124@gmail.com>
2014-10-11 12:02:52 -0700

    Project Created

---
+++ Beatles Hangman/Beatles Hangman/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Beatles Hangman
+//
+//  Created by Victoria Bian on 10/11/14.
+//  Copyright (c) 2014 Beatles Hangman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
